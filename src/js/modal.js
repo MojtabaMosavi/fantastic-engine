50121e120adf341efddfc8ba87df4faf70b5b48c
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2021-12-30 10:05:48 +0100

    completed the module for modal

---
+++ src/js/modal.js
@@ -0,0 +1,37 @@
+
+
+export const Modal = (function(){
+
+    // dom caching --------------------------------------------
+    const modal = document.querySelector(".modal");
+    const modalBtn = modal.querySelector(".modal__button");
+
+    // functions ----------------------------------------------
+    const _bindEvents = () =>{
+
+        // closing the modal 
+        modalBtn.addEventListener("click", deactivateModel)
+    }
+
+    function _init(){
+        _bindEvents()
+    }
+
+    const activateModal = () => {
+        modal.classList.add("modal--active")
+    }
+
+    const deactivateModel = () => {
+        modal.classList.remove("modal--active")
+    }
+
+    // initializing ----------------------------------------
+    _init()
+
+    // public api -------------------------------------------
+    return {
+        activateModal:activateModal,
+        deactivateModel:deactivateModel,
+    }
+})()
+
