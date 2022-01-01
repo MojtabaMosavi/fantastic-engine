18e0ba2f64034c4a219d6ac1e22a6f1bea500e8e
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2022-01-01 14:59:28 +0100

    Completed timing of transition to differnt pages.
    Added variables for timing of each transition.
    Added functions to determine the winner.
    Connected the modal to the rules btn.

--- src/js/game.js
+++ src/js/game.js
@@ -1,3 +1,4 @@
+import {Modal} from "./modal.js";
 
 export const game = (function(){
 
@@ -11,15 +12,20 @@ export const game = (function(){
     const gameHouseWrapper = game.querySelector(".game__house-wrapper")
     const gameHouse = game.querySelector(".game__house-button")
     const gameHouseBtn = game.querySelector(".game__house-button")
+    const gameWinnerText = game.querySelector(".game__winner");
+    const gameRulesBtn = game.querySelector(".game__rules");
+
     // variables ----------------------------------------------------
     const _gameBodyAnimationTime = 1500;
     const _HouseChoiceAnimationTime = 2000; 
     const _houseAnimationRunTime = 1700;
     const _houseAnimationsConstant = 1800;
+    const _computerChoiceDisplayDelay = 2500;
 
     const _gameChoices = ["scissors","paper","rock","spock","lizard"];
-    let _playChoice;
+    let _playerChoice;
     let _houseChoice;
+    let _winner;
     let _randomIntLoop;
 
 
@@ -35,21 +41,37 @@ export const game = (function(){
         gameBtns.forEach(btn => {
             btn.addEventListener("click", () => {
                 bodyAnimateOut();
+                // record play choice 
+                _playerChoice = btn.dataset.value; 
                 // when the exit animation is run 
                 setTimeout(()=> {
                     hideGameBody();
-                    // record play choice 
-                    _playChoice = btn.dataset.value; 
-                    gameResultSetState(gamePlayer,"player",_playChoice);
+                    gameResultSetState(gamePlayer,"player",_playerChoice);
                     showGameResult();
                 },_gameBodyAnimationTime)
+
+                // simulate house choocing randomly 
                 simulateHouseChoices()
+
+                // show the house's choice after simulating the choice is done 
                 setTimeout(() => {
-                    resultRemoveEmptyCircle()
+                    resultRemoveEmptyCircle();
                     gameResultSetState(gameHouse,"house",_houseChoice);
-                },_randomIntLoop * 2500)
+                },_randomIntLoop * _computerChoiceDisplayDelay);
+                
+                // show the final state of result 
+                setTimeout(()=> {
+                    resultFinalState();
+                }, _randomIntLoop * _computerChoiceDisplayDelay + 1000);
+
+                
+                decideWinner(gamePlayerRules(),_playerChoice,_houseChoice)
+                console.log("the winner is",_winner);
 
             })
+
+            // behavior of the ruels button 
+            gameRulesBtn.addEventListener("click", Modal.activateModal);
         })
     }
 
@@ -83,7 +105,7 @@ export const game = (function(){
         element.classList.add(`game__${player}-button--${state}`);
     }
 
-    const gameResultRemoveState = (element,state) => {
+    const gameResultRemoveState = (element,player,state) => {
         element.classList.remove(`game__button--${state}`);
         element.classList.remove(`game__${player}-button--${state}`);
     }
@@ -104,14 +126,65 @@ export const game = (function(){
         gameHouse.classList.add("game__house-button--circle-empty")
     }
 
+    // transitioning to result's final state 
+    const resultFinalState = () => {
+        gameResult.classList.remove("game__result--intro")
+    } 
+
+    // To determine the winner we only need to cover scenarios where the player or house wins
+    const gamePlayerRules = () => {
+        // all the scenarios that lear to the player winning 
+        let rules = [
+            {playerChoice:"scissors",houseChoice: "paper",winner:"player"},
+            {playerChoice:"scissors",houseChoice: "lizard",winner:"player"},
+            {playerChoice:"paper",houseChoice: "rock",winner:"player"},
+            {playerChoice:"paper",houseChoice: "spock",winner:"player"},
+            {playerChoice:"rock",houseChoice: "lizard",winner:"player"},
+            {playerChoice:"rock",houseChoice: "scissors",winner:"player"},
+            {playerChoice:"lizard",houseChoice: "spock",winner:"player"},
+            {playerChoice:"lizard",houseChoice: "paper",winner:"player"},
+            {playerChoice:"spock",houseChoice: "scissors",winner:"player"},
+            {playerChoice:"spock",houseChoice: "rock",winner:"player"},
+            ]
+        return rules;
+    }
+
+    // deciding the winner 
+    const decideWinner = (rules,player,house) => {
+        if(player === house){
+            _winner = "none";
+        } else{
+            // if you find a match for the condition set _winner and exit
+            for (const item of rules){
+                let {playerChoice,houseChoice,winner} = item;
+                if(playerChoice === player && houseChoice === house ){
+                    _winner = winner;
+                    return;
+                }
+                _winner = "house";
+            }
+        }
+    }
+
+    const gameUpdateWinnerText = () => {
+        let text = (_winner === "player") ? "player" : (_winner === "house") ? "house" : "tie";
+        return text;
+    }
+
+
+    // updating the winner text 
+    const updateGameWinner = () => {
+
+    }
+
     // includes min but not max 
     const getRamdomInt = (minv,maxv) => {
         let min = Math.ceil(minv);
         let max = Math.floor(maxv)
         return Math.floor(Math.random() * (max - min) + min)
-    } 
+    }
 
-
+    // simulate house chosing a randomly 
     const simulateHouseChoices= () => {
         _randomIntLoop = getRamdomInt(5,10);
         let computerChoices=  []
@@ -125,8 +198,9 @@ export const game = (function(){
             },_HouseChoiceAnimationTime + (_houseAnimationsConstant * i))
         }
         _houseChoice = computerChoices.pop();
+    
     }
-
+    // initialization ---------------------------------------------------
     _init()
 
 
