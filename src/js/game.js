181931f8e2b070b68b7bd32a245886acb1e7c5ef
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2022-01-02 15:09:38 +0100

    completed all the game functionality

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import {Modal} from "./modal.js";
 
 export const game = (function(){
+    document.addEventListener("DOMContentLoaded",()=> {
 
     // dom caching-----------------------------------------------------
     const game = document.querySelector(".game");
@@ -9,11 +10,14 @@ export const game = (function(){
     const gameBody = game.querySelector(".game__body");
     const gameResult = game.querySelector(".game__result");
     const gamePlayer = game.querySelector(".game__player-button");
-    const gameHouseWrapper = game.querySelector(".game__house-wrapper")
+    const gameHouseWrapper = game.querySelector(".game__house-wrapper");
+    const gamePlayerWrapper = game.querySelector(".game__player-wrapper");
     const gameHouse = game.querySelector(".game__house-button")
     const gameHouseBtn = game.querySelector(".game__house-button")
     const gameWinnerText = game.querySelector(".game__winner");
     const gameRulesBtn = game.querySelector(".game__rules");
+    const gameScore = game.querySelector(".game__score");
+    const gameReplayBtn = game.querySelector(".game__replay");
 
     // variables ----------------------------------------------------
     const _gameBodyAnimationTime = 1500;
@@ -27,71 +31,142 @@ export const game = (function(){
     let _houseChoice;
     let _winner;
     let _randomIntLoop;
+    let _score = 0;
 
 
 
     // functions--------------------------------------------------------
     const _init = () => {
+        _updateScoreDisplay();
         _bindEvents()
     }
 
     const _bindEvents = () => {
 
         // click evenets on game buttons ------------------------------
-        gameBtns.forEach(btn => {
-            btn.addEventListener("click", () => {
-                bodyAnimateOut();
-                // record play choice 
-                _playerChoice = btn.dataset.value; 
-                // when the exit animation is run 
-                setTimeout(()=> {
-                    hideGameBody();
-                    gameResultSetState(gamePlayer,"player",_playerChoice);
-                    showGameResult();
-                },_gameBodyAnimationTime)
-
-                // simulate house choocing randomly 
-                simulateHouseChoices()
-
-                // show the house's choice after simulating the choice is done 
-                setTimeout(() => {
-                    resultRemoveEmptyCircle();
-                    gameResultSetState(gameHouse,"house",_houseChoice);
-                },_randomIntLoop * _computerChoiceDisplayDelay);
-                
-                // show the final state of result 
-                setTimeout(()=> {
-                    resultFinalState();
-                }, _randomIntLoop * _computerChoiceDisplayDelay + 1000);
-
-                
-                decideWinner(gamePlayerRules(),_playerChoice,_houseChoice)
-                console.log("the winner is",_winner);
-
-            })
-
-            // behavior of the ruels button 
-            gameRulesBtn.addEventListener("click", Modal.activateModal);
+        gameBtns.forEach(btn => { 
+            btn.addEventListener("click", () => {_startGame(btn)});
         })
+        
+        // behavior of the ruels button --------------------------------------
+        gameRulesBtn.addEventListener("click", Modal.activateModal);
+        
+        // replay button ---------------------------------------------------
+        gameReplayBtn.addEventListener("click",_gameReset)
     }
 
+    const _startGame = (btn)=>{
+        bodyAnimateOut();
+        // record play choice 
+        _playerChoice = btn.dataset.value; 
+        // when the exit animation is run 
+        setTimeout(()=> {
+            hideGameBody();
+            _gameResultSetState(gamePlayer,"player",_playerChoice);
+            showGameResult();
+        },_gameBodyAnimationTime)
 
-    const bodyAnimateOut = () =>{
-        gameBody.classList.add("game__body--hidden");
+        // simulate house choocing randomly 
+        _simulateHouseChoices()
+
+        // show the house's choice after simulating the choice is done 
+        setTimeout(() => {
+            _resultRemoveEmptyCircle();
+            _gameResultSetState(gameHouse,"house",_houseChoice);
+        },_randomIntLoop * _computerChoiceDisplayDelay);
+        
+        // show the final state of result 
+        setTimeout(()=> {
+            _decideWinner(getPlayerRules(),_playerChoice,_houseChoice)
+            _updateWinnerText();
+            _updateScoreDisplay();
+            _resultRemoveFirstState();
+            _AddWinnerAnimation(_winner);
+
+        }, _randomIntLoop * _computerChoiceDisplayDelay + 1000);
+    }
+
+    // restart game resets t its initial state
+    const _gameReset = () => {
+        _updateScoreDisplay();
+        // resets states set on body 
+        bodyAnimateIn();
+        showGameBody();
+        // remove the animation set on the winner 
+        _removeWinnerAnimation(_winner);
+        // remove the state set on this elements 
+        _gameResultRemoveState(gamePlayer,"player",_playerChoice);
+        _gameResultRemoveState(gameHouse,"house",_houseChoice);
+        _resultAddEmptyCircle();
+        // reset states set on the result section 
+        hideGameResult();
+        _resultAddFirstState();
+        _resultAddFinalState();
 
     }
-    const bodyAnimateIn = () =>{
-        gameBody.classList.remove("game__body--hidden");
-
-    }
-
+ 
+    // hidding and showing the body = first state of the game with and without aniamtion
     const hideGameBody = () => {
         gameBody.classList.add("hidden")
     }
 
     const showGameBody = () => {
         gameBody.classList.remove("hidden")
+    }    
+    
+    const bodyAnimateOut = () =>{
+        gameBody.classList.add("game__body--hidden");
     }
+
+    const bodyAnimateIn = () =>{
+        gameBody.classList.remove("game__body--hidden");
+    }
+
+    // Setting and removing the state a btn 
+    const _gameResultSetState = (element,player,state) => {
+        element.classList.add(`game__button--${state}`);
+        element.classList.add(`game__${player}-button--${state}`);
+    }
+
+    const _gameResultRemoveState = (element,player,state) => {
+        element.classList.remove(`game__button--${state}`);
+        element.classList.remove(`game__${player}-button--${state}`);
+    }
+
+    // removing and adding the circle on right side on result__game--intro
+    const _resultRemoveEmptyCircle = () => {
+        gameHouse.classList.remove("game__house-button--circle-empty")
+    }
+
+    const _resultAddEmptyCircle = () => {
+        gameHouse.classList.add("game__house-button--circle-empty")
+    }
+
+    const _houseBtnAddState = (state) =>{
+        gameHouse.classList.add(`game__house-button--circle-${state}`)
+    }
+
+    const _houseBtnRemoveState = (state) =>{
+        gameHouse.classList.remove(`game__house-button--circle-${state}`)
+    }
+    // tr
+    // transitioning to result's final state 
+    const _resultAddFirstState = () => {
+        gameResult.classList.add("game__result--intro")
+    }
+
+    const _resultRemoveFirstState = () => {
+        gameResult.classList.remove("game__result--intro");
+    }
+
+    const _resultAddFinalState = () => {
+        gameResult.classList.add("game__result--active");
+    }
+
+    const _resultRemoveFinalState = () => {
+        gameResult.classList.remove("game__result--active");
+    }
+    // hidding and showing the result section = second state of the game
     const hideGameResult = () => {
         gameResult.classList.add("hidden")
     }
@@ -100,39 +175,9 @@ export const game = (function(){
         gameResult.classList.remove("hidden")
     }
 
-    const gameResultSetState = (element,player,state) => {
-        element.classList.add(`game__button--${state}`);
-        element.classList.add(`game__${player}-button--${state}`);
-    }
-
-    const gameResultRemoveState = (element,player,state) => {
-        element.classList.remove(`game__button--${state}`);
-        element.classList.remove(`game__${player}-button--${state}`);
-    }
-
-    const houseBtnAddState = (state) => {
-        gameHouseBtn.classList.add(`game__house-button--circle-${state}`);
-    }
-
-    const houseBtnRemoveState = (state) => {
-        gameHouseBtn.classList.remove(`game__house-button--circle-${state}`);
-    }
-
-    const resultRemoveEmptyCircle = () => {
-        gameHouse.classList.remove("game__house-button--circle-empty")
-    }
-
-    const resultAddEmptyCircle = () => {
-        gameHouse.classList.add("game__house-button--circle-empty")
-    }
-
-    // transitioning to result's final state 
-    const resultFinalState = () => {
-        gameResult.classList.remove("game__result--intro")
-    } 
 
     // To determine the winner we only need to cover scenarios where the player or house wins
-    const gamePlayerRules = () => {
+    const getPlayerRules = () => {
         // all the scenarios that lear to the player winning 
         let rules = [
             {playerChoice:"scissors",houseChoice: "paper",winner:"player"},
@@ -150,7 +195,7 @@ export const game = (function(){
     }
 
     // deciding the winner 
-    const decideWinner = (rules,player,house) => {
+    const _decideWinner = (rules,player,house) => {
         if(player === house){
             _winner = "none";
         } else{
@@ -159,22 +204,41 @@ export const game = (function(){
                 let {playerChoice,houseChoice,winner} = item;
                 if(playerChoice === player && houseChoice === house ){
                     _winner = winner;
+                    _score += 1;
+                    localStorage.setItem("score",_score);
                     return;
                 }
-                _winner = "house";
             }
+            _winner = "house";
+            _score -= 1;
+            localStorage.setItem("score",_score);
+
         }
     }
 
-    const gameUpdateWinnerText = () => {
-        let text = (_winner === "player") ? "player" : (_winner === "house") ? "house" : "tie";
-        return text;
+    const _updateWinnerText = () => {
+        let text = (_winner === "player") ? "you win" : (_winner === "house") ? "you lose" : "it's a Tie";
+        gameWinnerText.textContent = text; 
     }
 
+    const _updateScoreDisplay = () => {
+        gameScore.innerText = localStorage.getItem("score");
+    }
 
-    // updating the winner text 
-    const updateGameWinner = () => {
-
+    // adding and removing winner animations
+    const _AddWinnerAnimation = (winner)=> {
+        if(winner !== "none"){
+            (winner === "player") ? 
+            gamePlayerWrapper.classList.add("game__player-wrapper--winner"):
+            gameHouseWrapper.classList.add("game__house-wrapper--winner");
+        }
+    }
+    const _removeWinnerAnimation= (winner)=> {
+        if(winner !== "none"){
+            (winner === "player") ? 
+            gamePlayerWrapper.classList.remove("game__player-wrapper--winner"):
+            gameHouseWrapper.classList.remove("game__house-wrapper--winner");
+        }
     }
 
     // includes min but not max 
@@ -185,7 +249,7 @@ export const game = (function(){
     }
 
     // simulate house chosing a randomly 
-    const simulateHouseChoices= () => {
+    const _simulateHouseChoices= () => {
         _randomIntLoop = getRamdomInt(5,10);
         let computerChoices=  []
         for(let i=0; i <= _randomIntLoop; i++) {
@@ -193,20 +257,26 @@ export const game = (function(){
             let choice =  _gameChoices[getRamdomInt(1,5)];
             computerChoices.push(choice)
             setTimeout(()=> {
-                houseBtnAddState(choice);
-                setTimeout(()=>{houseBtnRemoveState(choice)}, _houseAnimationRunTime)
+                _houseBtnAddState(choice);
+                setTimeout(()=>{_houseBtnRemoveState(choice)}, _houseAnimationRunTime)
             },_HouseChoiceAnimationTime + (_houseAnimationsConstant * i))
         }
         _houseChoice = computerChoices.pop();
-    
     }
+    
     // initialization ---------------------------------------------------
     _init()
 
 
     // public API--------------------------------------------------------
     return{
-
+        hideGameBody:hideGameBody,
+        showGameBody:showGameBody,
+        bodyAnimateIn:bodyAnimateIn,
+        bodyAnimateOut:bodyAnimateOut,
+        hideGameResult:hideGameResult,
+        showGameResult:showGameResult,
+        getPlayerRules:getPlayerRules,
+        getRamdomInt:getRamdomInt,
     }
-    
-})()
+})})()
